748c71b005115e5d8905b5f9961a32267e3dc421
Danny <dannyaguirre09@hotmail.com>
2020-09-13 18:03:41 -0500

    'Preenvio'

---
+++ Controllers/PreenvioController.cs
@@ -0,0 +1,33 @@
+﻿using SysFloricola.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SysFloricola.Controllers
+{
+    public class PreenvioController : Controller
+    {
+		private PreenvioDAL objDAL = new PreenvioDAL();
+
+        // GET: Preenvio
+        public ActionResult Index()
+        {
+			List<PREENVIOS> lista = objDAL.Lista_Preenvios();
+            return View(lista);
+        }
+
+		public ActionResult Preenvio()
+		{
+			ViewBag.Fecha = DateTime.Now.ToShortDateString();
+			return View();
+		}
+
+		public ActionResult ModalCliente()
+		{
+			List<CLIENTES> listaClientes = ClientesDAL.Obtener_Clientes();
+			return View(listaClientes);
+		}
+    }
+}
---
+++ Models/PreenvioDAL.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SysFloricola.Models
+{
+	public class PreenvioDAL
+	{
+		public List<PREENVIOS> Lista_Preenvios()
+		{
+			List<PREENVIOS> lista = new List<PREENVIOS>();
+			using (BDFloricolaContext db = new BDFloricolaContext())
+			{
+				lista = db.PREENVIOS.OrderByDescending(x => x.PREFECHA).ToList();
+			}
+			return lista;
+		}
+	}
+}
